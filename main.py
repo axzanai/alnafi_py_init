accacfcbcf9de2e065eb2a094c627b92b61a8c5a
Muhammad Arslan <axzansolutions@gmail.com>
2024-12-05 23:44:06 +0500

    Add initial files: .gitignore README.md requirements.txt main.py

--- main.py
+++ main.py
@@ -1,3 +1,43 @@
-import sum_2
+# my_dic = {'first':1, 'second': 2}
+# if "second" in my_dic:
+#     print("Key 'second' exist")
 
-sum_2.sum2(3, 5)
+
+# list = [1 , "baharat", 2.3]
+
+# list.append("jugnu")
+# print(list)
+
+# try:
+#     x = 1/0
+# except ZeroDivisionError:
+#     print("You can't divide by zero!")
+
+# with open("main.py", "r") as file:
+#     content = file.read()
+#     print(content)
+
+
+# with open("main.py", "w") as file:
+#     content = file.write("print('I guess Im in love with coading')")
+#     print(content)
+    
+# print('I guess Im in love with coading')
+
+# x = [1, 2, 3]
+# y = x
+# y[0] = 10
+
+# print(x.pop(2))
+# print(x)
+
+name = "ArslAN"
+print(name.lower())
+
+
+dic = {"std1": "Maaz Shah", "std2": "Zayyan Khan", "std3":"Maaz Ahmed"}
+print(dic.get("std2"))
+
+from math import sqrt
+print(sqrt(2))
+
