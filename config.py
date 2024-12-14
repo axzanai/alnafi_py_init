b6a1b6da82e206a2a8dcb3bf6c192c522cf0a4d3
axzanai <axzansolutions@gmail.com>
2024-12-15 02:55:31 +0500

    update files

--- config.py
+++ config.py
@@ -52,30 +52,14 @@ def remove_file(filename):
     except Exception as e:
         print(f"An unexpected error occured: {e}")
     
-# def remove_file(filename):
-#     try:
-#         os.remove(filename)
-#         print(f"The file {filename} has been removed.")
-#     except FileNotFoundError:
-#         print(f"The file {filename} does not exist.")
-        
 
-# def create_file(filename = input("Enter file name?")):
-#     try:
-#         with open(filename, "w") as f:
-#             f.write("")
-#     except Exception as e:
-#         print(f"An error occured: {e}")
+def read_file(filename = input("Enter file name?")):
+    try:
+        with open(filename) as f:
+            content = f.read()
+        print(content)
+    except Exception as e:
+        print(f"Unexpected error: {e}")
         
-# # create_file()
-
-# def read_file(filename = input("Enter file name?")):
-#     try:
-#         with open(filename) as f:
-#             content = f.read()
-#         print(content)
-#     except Exception as e:
-#         print(f"Unexpected error: {e}")
-        
-# read_file()
+read_file()
 
